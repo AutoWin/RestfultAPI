9615196385bbf9a5b82ffb37e0642b69cae85a0f
Autowin <luongthanhkx1997@gmail.com>
2020-12-02 10:13:04 +0000

    update

--- api-c6/config.py
+++ api-c6/config.py
@@ -1,6 +1,5 @@
 import os
 
-
 # You need to replace the next values with the appropriate values for your configuration
 basedir = os.path.abspath(os.path.dirname(__file__))
 DEBUG = True
@@ -8,5 +7,10 @@ PORT = 5000
 HOST = "127.0.0.1"
 SQLALCHEMY_ECHO = False
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-SQLALCHEMY_DATABASE_URI = "postgresql://{DB_USER}:{DB_PASS}@{DB_ADDR}/{DB_NAME}".format(DB_USER="postgres", DB_PASS="123456", DB_ADDR="127.0.0.1", DB_NAME="messages")
+SQLALCHEMY_DATABASE_URI = "postgresql://{DB_USER}:{DB_PASS}@{DB_ADDR}/{DB_NAME}".format(DB_USER="postgres",
+                                                                                        DB_PASS="123456",
+                                                                                        DB_ADDR="127.0.0.1",
+                                                                                        DB_NAME="messages")
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+PAGINATION_PAGE_SIZE = 5
+PAGINATION_PAGE_ARGUMENT_NAME = 'page'
